6241fb073f09adc65100e96e963af80cf912cd27
AntonioAdelino <antonio.asn03@gmail.com>
2021-02-12 14:03:20 -0300

    Refatorando código

--- src/main/java/com/servidorsloc/controller/GerenteController.java
+++ src/main/java/com/servidorsloc/controller/GerenteController.java
@@ -8,16 +8,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.annotation.RequestScope;
-
 import com.servidorsloc.service.GerenteServices;
-import com.servidorsloc.service.VendedorServices;
 import com.servidorsloc.model.Gerente;
  
 
@@ -26,9 +20,6 @@ public class GerenteController {
 	
 	@Autowired
 	private GerenteServices gerenteServices;
-	
-	@Autowired
-	private VendedorServices vendedorServices;
 	
 	@GetMapping("/gerentes")
 	public List<Gerente> listAll() {    
